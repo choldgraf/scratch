ccf04dfa75b0ed0fd0b9c5257a1226540f00569f
Chris Holdgraf <choldgraf@berkeley.edu>
2022-07-04 16:45:54 +0200

    Sphinx

--- noxfile.py
+++ noxfile.py
@@ -7,11 +7,11 @@ nox.options.reuse_existing_virtualenvs = True
 @nox.session
 def docs(session):
     session.install("-r", "requirements.txt")
-    session.run("sphinx-build", "-b=html", "docs/", "docs/_build/html")
+    session.run("sphinx-build", "-b=html", "docs/", "docs/_build/html", *session.posargs)
 
 
 @nox.session(name="docs-live")
 def docs_live(session):
     session.install("-r", "requirements.txt")
     session.install("sphinx-autobuild")
-    session.run("sphinx-autobuild", "docs", "docs/_build/html")
+    session.run("sphinx-autobuild", "docs", "docs/_build/html", *session.posargs)
